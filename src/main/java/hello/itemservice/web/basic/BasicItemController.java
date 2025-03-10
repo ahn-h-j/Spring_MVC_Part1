59eef7759663d7d5a3a2f228d3ec4fa75a78eba8
ahn_h_j <tkgkd246@gmail.com>
2025-03-10 11:33:51 +0900

    feat : 상품등록 기능 추가
    BasicItemController.java
    - save() 함수 : 상품 등록 폼에서 데이터를 받아온 후 상품 정보 저장
    - PostMapping : /add
    - basic/item 호출
    
    addForm.html
    - 상품 정보를 등록하기 위한 화면

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -4,14 +4,14 @@ import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
@@ -33,7 +33,46 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/add")
+    private String addForm(){
+        return "basic/addForm";
+    }
 
+//    @PostMapping("/add")
+    private String saveV1(@RequestParam String itemName, @RequestParam int price, @RequestParam Integer quantity, Model model){
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+        itemRepository.save(item);
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    private String saveV2(@ModelAttribute("item") Item item){
+
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    private String saveV3(@ModelAttribute Item item){
+
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    private String saveV4(Item item){
+
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
 
     @PostConstruct
     public void init() {
